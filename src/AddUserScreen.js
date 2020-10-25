0c0ee3bc32e41221cd3750656526ec7339685412
Mitchell Adam <mbadam@ualberta.ca>
2020-10-25 12:24:42 -0700

    fix add user screen

--- src/AddUserScreen.js
+++ src/AddUserScreen.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 import {FirebaseProvider} from './Firebase';
-import { styles } from './App'
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+import styles from './Styles'
 
 export default function AddUserScreen({route, navigation}) {
     const [firstNameText, setFirstNameText] = useState();
@@ -30,27 +32,72 @@ export default function AddUserScreen({route, navigation}) {
     }
 
     return (
-        <View>
-            <TextInput
-                placholder="First Name"
-                onChangeText={text => setFirstNameText(text)}
-                defaultValue={firstNameText}
-            />
-            <TextInput
-                placholder="Last Name"
-                onChangeText={text => setLastNameText(text)}
-                defaultValue={lastNameText}
-            />
-            <TextInput
-                placholder="Personal Health Number"
-                onChangeText={text => setPhnText(text)}
-                defaultValue={phnText}
-            />
-            <TextInput
-                placholder="Health Insurance Number"
-                onChangeText={text => setHinText(text)}
-                defaultValue={hinText}
-            />
+        <View style={styles.container}>
+            <View style={addressStyles.row}>
+                <Text style={styles.regularText}>First Name</Text>
+                <TextInput
+                    height='40%'
+                    width='65%'
+                    selectionColor='#4183e0'
+                    underlineColorAndroid='#4183e0'
+                    placholder="First Name"
+                    onChangeText={text => setFirstNameText(text)}
+                    defaultValue={firstNameText}
+                />
+            </View>
+            <View style={addressStyles.row}>
+                <Text style={styles.regularText}>Last Name</Text>
+                <TextInput
+                    height='40%'
+                    width='65%'
+                    selectionColor='#4183e0'
+                    underlineColorAndroid='#4183e0'
+                    placholder="Last Name"
+                    onChangeText={text => setLastNameText(text)}
+                    defaultValue={lastNameText}
+                />
+            </View>
+            <View style={addressStyles.row}>
+                <Text style={styles.regularText}>Health Number</Text>
+                <TextInput
+                    height='40%'
+                    width='65%'
+                    selectionColor='#4183e0'
+                    underlineColorAndroid='#4183e0'
+                    placholder="Personal Health Number"
+                    onChangeText={text => setPhnText(text)}
+                    defaultValue={phnText}
+                />
+            </View>
+            <View style={addressStyles.row}>
+                <Text style={styles.regularText}>Insurance Number</Text>
+                <TextInput
+                    height='40%'
+                    width='65%'
+                    selectionColor='#4183e0'
+                    underlineColorAndroid='#4183e0'
+                    placholder="Health Insurance Number"
+                    onChangeText={text => setHinText(text)}
+                    defaultValue={hinText}
+                />
+            </View>
+            <TouchableOpacity
+                style={styles.setAddressButton}
+                onPress={() => handleSaveUser()}
+            >
+                <Text style={styles.btnText}>Save User</Text>
+            </TouchableOpacity>
         </View>
     );
 }
+
+
+const addressStyles = StyleSheet.create({
+    row: {
+        flex: 1,
+        flexDirection: 'row',
+        width: '80%',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    }
+});
